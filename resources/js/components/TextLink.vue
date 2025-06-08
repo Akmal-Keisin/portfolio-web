db1c649d285c8b8c16e74016d43f405eb772920b
Akmal-Keisin <akmalkeisin@gmail.com>
2025-06-08 21:11:18 +0700

    init

---
+++ resources/js/components/TextLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { InertiaLinkProps, Link } from '@inertiajs/vue3';
+interface Props extends Pick<InertiaLinkProps, "href" | "method" | "as"> {
+    tabindex?: number;
+};
+defineProps<Props>();
+</script>
+
+<template>
+    <Link
+        :href="href"
+        :tabindex="tabindex"
+        :method="method"
+        :as="as"
+        class="text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500"
+    >
+        <slot />
+    </Link>
+</template>
